ac526ce01c8d868784ce2ae9232382a19b70c673
Benjamin TURPIN <benjamin.turpin@etu.umontpellier.fr>
2024-09-04 15:40:41 +0200

    ajout constructeur

--- src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -3,6 +3,7 @@ package fr.iut.editeur.commande;
 import fr.iut.editeur.document.Document;
 
 public class CommandeRemplacer extends CommandeDocument{
+
     public CommandeRemplacer(Document document, String[] parameters) {
         super(document, parameters);
     }
