dbc43d8434557848fca39c11d764aa4923efad16
Benjamin TURPIN <benjamin.turpin@etu.umontpellier.fr>
2024-09-04 15:44:55 +0200

    ajout executer

--- src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -7,4 +7,15 @@ public class CommandeRemplacer extends CommandeDocument{
     public CommandeRemplacer(Document document, String[] parameters) {
         super(document, parameters);
     }
+
+    @Override
+    public void executer(){
+        if(parameters.length < 3 || parameters.length > 5){
+            System.err.println("Format attendu : remplacer;nbDebut;nbFin;texte");
+            return;
+        }
+        String texte = parameters[3];
+        this.document.remplacer(Integer.parseInt(parameters[1]),Integer.parseInt(parameters[2]),texte);
+        super.executer();
+    }
 }
