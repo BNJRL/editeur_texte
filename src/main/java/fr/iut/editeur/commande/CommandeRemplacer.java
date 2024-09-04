3ca147fef1b03be5fbfedff4d4d2d34216ceffad
Benjamin TURPIN <benjamin.turpin@etu.umontpellier.fr>
2024-09-04 15:38:41 +0200

    création de CommandeRemplacer.java

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,9 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument{
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+}
